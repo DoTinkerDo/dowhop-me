c6443e72295342c61957bc57bb62b7f10f311bde
JohannBillar <jwbillar@gmail.com>
2017-07-18 18:07:57 -0700

    Make fonts responsive

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { Grid, Row } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 // import PropTypes from 'prop-types';
 
 function App() {
   return (
     <Grid>
       <Row>
-        <div className="site-content">
-          <p>A bunch of paragraph text goes here.</p>
-          <h1>h1. Bootstrap heading</h1>
-          <h2>h2. Bootstrap heading</h2>
-          <h3>h3. Bootstrap heading</h3>
-          <h4>h4. Bootstrap heading</h4>
-          <h5>h5. Bootstrap heading</h5>
-          <h6>h6. Bootstrap heading</h6>
-          <blockquote>I am a blockquote</blockquote>
-          <pre>pre stuff goes here</pre>
-          <code>code stuff goes here</code>
-          <big>big stuff goes here</big>
-        </div>
+        <Col md={12}>
+          <div className="site-content">
+            <div className="site-main">
+              <p>A bunch of paragraph text goes here.</p>
+              <h1>h1. Bootstrap heading</h1>
+              <h2>h2. Bootstrap heading</h2>
+              <h3>h3. Bootstrap heading</h3>
+              <h4>h4. Bootstrap heading</h4>
+              <h5>h5. Bootstrap heading</h5>
+              <h6>h6. Bootstrap heading</h6>
+              <blockquote>I am a blockquote</blockquote>
+              <pre>pre stuff goes here</pre>
+              <code>code stuff goes here</code>
+              <big>big stuff goes here</big>
+            </div>
+          </div>
+        </Col>
       </Row>
     </Grid>
   );
