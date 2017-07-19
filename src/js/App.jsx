5865b54f70ad8ffa0d2cf80742764cca3069d387
JohannBillar <jwbillar@gmail.com>
2017-07-19 14:54:29 -0700

    Configure ESLint with Flow plugins

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,35 +1,11 @@
+// @flow
+
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
-// import PropTypes from 'prop-types';
+import Container from './Container';
 
-function App() {
-  return (
-    <Grid>
-      <Row>
-        <Col md={12}>
-          <div className="site-content">
-            <div className="site-main">
-              <p>A bunch of paragraph text goes here.</p>
-              <h1>h1. Bootstrap heading</h1>
-              <h2>h2. Bootstrap heading</h2>
-              <h3>h3. Bootstrap heading</h3>
-              <h4>h4. Bootstrap heading</h4>
-              <h5>h5. Bootstrap heading</h5>
-              <h6>h6. Bootstrap heading</h6>
-              <blockquote>I am a blockquote</blockquote>
-              <pre>pre stuff goes here</pre>
-              <code>code stuff goes here</code>
-              <big>big stuff goes here</big>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Grid>
-  );
-}
-
-// App.propTypes = {
-//   message: PropTypes.string.isRequired
-// };
+const App = () =>
+  <Container>
+    <p>Hello World!</p>
+  </Container>;
 
 export default App;
---
+++ src/js/Container.jsx
@@ -0,0 +1,19 @@
+// @flow
+
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+const Container = ({ children }: Object) =>
+  <Grid>
+    <Row>
+      <Col md={12}>
+        <div className="site-content">
+          <div className="site-main">
+            {children}
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </Grid>;
+
+export default Container;
