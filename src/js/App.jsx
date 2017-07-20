98f176817e4764a07e084cdb360074794e2e4693
JohannBillar <jwbillar@gmail.com>
2017-07-20 09:57:17 -0700

    Rename Container to Wrapper

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,12 +1,12 @@
 // @flow
 
 import React from 'react';
-import Container from './Container';
+import Wrapper from './Wrapper';
 
 const App = () =>
-  <Container>
+  <Wrapper>
     <h1>Welcome to DoWhop!</h1>
     <p>Do What you Love</p>
-  </Container>;
+  </Wrapper>;
 
 export default App;
