bdda3f887c7d1958aca27e0143c9399f4aa1c19f
JohannBillar <jwbillar@gmail.com>
2017-07-20 08:42:35 -0700

    Clean up code

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import { render } from 'react-dom';
 import App from './js/App';
 
 require('./sass/main.scss');
-// import styles from './sass/main.scss';
 
 const renderApp = () => {
   render(<App />, document.getElementById('root'));
