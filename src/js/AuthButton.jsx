198881ff3a7ec54eaed115cdb57015527ec847fa
JohannBillar <jwbillar@gmail.com>
2017-07-31 16:50:17 -0700

    Refactor login/logout button

--- src/js/AuthButton.jsx
+++ src/js/AuthButton.jsx
@@ -1,24 +1,23 @@
 // @flow
 
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import appAuth from './appAuth';
 
 const AuthButton = withRouter(
   ({ history }) =>
     appAuth.isAuthenticated
-      ? <p>
-          Welcome!{' '}
-          <Button
-            onClick={() => {
-              appAuth.signout(() => history.push('/'));
-            }}
-          >
-            Log out
-          </Button>
-        </p>
-      : <p>You are not logged in.</p>
+      ? <Button
+          onClick={() => {
+            appAuth.signout(() => history.push('/'));
+          }}
+        >
+          Logout
+        </Button>
+      : <Link to="/login">
+          <Button>Login</Button>
+        </Link>
 );
 
 export default AuthButton;
