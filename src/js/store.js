ae5cc977261cf8a66116d89103a8f0d8d6012849
JohannBillar <jwbillar@gmail.com>
2017-08-16 14:16:49 -0700

    Revert Redux dev tools to older version for Safari compatibility

--- src/js/store.js
+++ src/js/store.js
@@ -12,11 +12,13 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    typeof window === 'object' &&
-    typeof window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__() !== 'undefined'
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      : f => f
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // typeof window === 'object' &&
+    // typeof window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    // window.__REDUX_DEVTOOLS_EXTENSION__() !== 'undefined'
+    //   ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //   : f => f
   )
 );
 /* eslint-enable */
